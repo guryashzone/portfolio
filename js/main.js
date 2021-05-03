2d6169294fca36585ec4344596c09e7c0c51b957
guryashzone <46774163+guryashzone@users.noreply.github.com>
2021-05-03 23:21:44 +0530

    Some changes

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ downloadBtn.addEventListener('click', () => {
 	const resume = document.getElementById('resume');
 	console.log(resume)
 	var opt = {
-	  margin:       1,
-	  filename:     'myfile.pdf',
-	  image:        { type: 'jpeg', quality: 0.98 },
-	  html2canvas:  { scale: 4 },
-	  jsPDF:        { format: 'a4', orientation: 'portrait' }
+	  margin: 0,
+	  filename: 'Guryash Singh - Resume.pdf',
+	  image: { type: 'jpeg', quality: 0.98 },
+	  html2canvas: { scale: 4 },
+	  jsPDF: { format: 'a4', orientation: 'portrait' }
 	};
 	
 	html2pdf(resume, opt);

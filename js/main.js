37298506241ae352292c03aeb2d418feb672faa6
guryashzone <46774163+guryashzone@users.noreply.github.com>
2023-05-06 13:41:52 +0530

    Data update

--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@ const downloadBtn = document.getElementById('download-pdf');
 
 downloadBtn.addEventListener('click', () => {
 	const resume = document.getElementById('resume');
-	console.log(resume)
 	var opt = {
-	  margin: 0,
-	  filename: 'Guryash Singh - Resume.pdf',
-	  image: { type: 'jpeg', quality: 0.98 },
-	  html2canvas: { scale: 4 },
-	  jsPDF: { format: 'a4', orientation: 'portrait' }
+		margin: 0,
+		filename: 'Guryash Singh - Resume.pdf',
+		image: { type: 'jpeg', quality: 0.98 },
+		html2canvas: { scale: 10 },
+		jsPDF: { format: 'a4', orientation: 'portrait' }
 	};
-	
+
 	html2pdf(resume, opt);
-})
+});
